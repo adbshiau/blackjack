061acb40fab7c7733f4d1ae62bf26885ad4fbb9e
ayashiau-ga <angelicashiau@gmail.com>
2022-03-19 10:01:31 -0700

    master deck added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
 console.log("JS file has loaded!");
+
+// DEFINE CONSTANTS
+const suits = ['s', 'c', 'd', 'h'];
+const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+const masterDeck = buildMasterDeck();
+
+
+// Define state variables
+
+// Set the elements on the DOM that needs to be updated
+
+// Add event listeners
+
+// Initial controller function
+
+// MASTER DECK
+function buildMasterDeck() {
+  const deck = [];
+  // Use nested forEach to generate card objects
+  suits.forEach(function(suit) {
+    ranks.forEach(function(rank) {
+      deck.push({
+        // The 'face' property maps to the library's CSS classes for cards
+        face: `${suit}${rank}`,
+        // Setting the 'value' property for game of blackjack, not war
+        value: Number(rank) || (rank === 'A' ? 11 : 10)
+      });
+    });
+  });
+  return deck;
+}
